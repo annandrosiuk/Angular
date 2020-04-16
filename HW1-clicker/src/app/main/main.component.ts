7bd4b545e3a9ca0b82c7142cd1f726d7af6592fa
annandrosiuk <ann.androsyuk@gmail.com>
2020-04-17 01:44:12 +0300

    general functionality done

--- HW1-clicker/src/app/main/main.component.ts
+++ HW1-clicker/src/app/main/main.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
-  @Input() name: string;
-  constructor() { }
+export class MainComponent {
+  counter: number = 0;
+  disabled = false;
+  delay: number = 3000;
+  speed: number =  this.counter / this.delay;
 
-  ngOnInit(): void {
+  @Input() name: string;
+  @Input() showMePartially: boolean;
+
+  getTimeout(event){
+    this.delay = event.target.value * 1000;
+    console.log(`it works, delay is ${this.delay}`)
   }
 
-  @Input() showMePartially: boolean;
+  increaseCounter() {
+    this.counter++;
+  }
+
+  timerStart(){
+    setTimeout(()=>{
+      this.disabled = true;
+      this.speed = this.counter / (this.delay / 1000);
+      // console.log('timeout');
+    }, this.delay);
+  }
+  
+  restartGame(){
+    this.counter = 0;
+    this.disabled = false;
+    console.log('restart');
+  }
 }
